4704006ec6dc0649a684b993793de5f407b35be0
Adhithyan Anilkumar <adhithyan2028@bca.ajce.in>
2026-09-24 20:21:00 +0530

    chore: initialize python virtual environment and backend dependencies

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from FastAPI"}
